da89891851244747f29ae824784a891c280419d9
Nicholxs <41819863+nichxlxs@users.noreply.github.com>
2018-07-28 20:53:02 +1000

    Create index.js

---
+++ index.js
@@ -0,0 +1,50 @@
+const botconfig = require("./botconfig.json");
+const Discord = require("discord.js");
+
+const bot = new Discord.Client({disableEveryone: true});
+
+bot.on("ready", async () => {
+    console.log(`${bot.user.name} is online!`);
+    bot.user.setActivity("$help | @nicholxs#7891");
+});
+
+bot.on("message", async message => {
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
+
+    let prefix = botconfig.prefix;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+
+    if(cmd === `${prefix}botinfo`){
+
+        let bicon = bot.user.displayAvatarURL;
+        let botembed = new Discord.RichEmbed()
+        .setDescription("Bot Information")
+        .setColor("#15f153")
+        .setThumbnail(bicon)
+        .addField("Bot Name", bot.user.username)
+        .addField("Created On", bot.user.createdAt);
+    
+        return message.channel.send(botembed);
+      }
+
+    if(cmd === `${prefix}serverinfo`){
+
+        let sicon = message.guild.iconURL;
+        let serverembed = new Discord.RichEmbed()
+        .setDescription("Server Information")
+        .setColor("#15f153")
+        .setThumbnail(sicon)
+        .addField("Server Name", message.guild.name)
+        .addField("Created On", message.guild.createdAt)
+        .addField("You Joined", message.member.joinedAt)
+        .addField("Total Members", message.guild.memberCount);
+    
+        return message.channel.send(serverembed);
+      }      
+
+});
+
+bot.login(botconfig.token);

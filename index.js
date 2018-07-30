5b31466b8945e994c33c4b52ca79f80197552749
Nicholxs <41819863+nichxlxs@users.noreply.github.com>
2018-07-30 20:26:31 +1000

    New CommandHandler!

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
-
+const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
+bot.commands = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+
+  if(err) console.log(err);
+
+  let jsfile = files.filter(f => f.split(".").pop() === "js");
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.");
+    return;
+  }
+
+  jsfile.forEach((f, i) => {
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    bot.commands.set(props.help.name, props);
+  });
+
+});
 
 bot.on("ready", async () => {
-    console.log(`${bot.user.name} is online!`);
+    console.log(`${bot.user.username} is online!`);
     bot.user.setActivity("$help | @nicholxs#7891");
 });
 
@@ -17,88 +36,9 @@ bot.on("message", async message => {
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
 
-    if(cmd === `${prefix}warn`){
-
-        //!warn @player askin for it
-    
-        let wUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-        if(!wUser) return message.channel.send("Can't find user!");
-        let wReason = args.join(" ").slice(22);
-        if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No can do pal!");
-        if(wUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
-    
-        let warnEmbed = new Discord.RichEmbed()
-        .setDescription("Warnings")
-        .setColor("#e56b00")
-        .addField("Warned User", `${wUser} with ID ${wUser.id}`)
-        .addField("Warned By", `<@${message.author.id}> with ID ${message.author.id}`)
-        .addField("Warned In", message.channel)
-        .addField("Time", message.createdAt)
-        .addField("Reason", wReason);
-    
-        let warnChannel = message.guild.channels.find(`name`, "incidents");
-        if(!warnChannel) return message.channel.send("Can't find incidents channel.");
-    
-        warnChannel.send(warnEmbed);
-    
-        return;
-      }
-
-    if(cmd === `${prefix}report`){
-
-        //$report @player this is the reason
-    
-        let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-        if(!rUser) return message.channel.send("Couldn't find user.");
-        let rreason = args.join(" ").slice(22);
-    
-        let reportEmbed = new Discord.RichEmbed()
-        .setDescription("Reports")
-        .setColor("#15f153")
-        .addField("Reported User", `${rUser} with ID: ${rUser.id}`)
-        .addField("Reported By", `${message.author} with ID: ${message.author.id}`)
-        .addField("Channel", message.channel)
-        .addField("Time", message.createdAt)
-        .addField("Reason", rreason);
-    
-        let reportschannel = message.guild.channels.find(`name`, "reports");
-        if(!reportschannel) return message.channel.send("Couldn't find reports channel.");
-    
-    
-        message.delete().catch(O_o=>{});
-        reportschannel.send(reportEmbed);
-    
-        return;
-      }
-
-    if(cmd === `${prefix}botinfo`){
-
-        let bicon = bot.user.displayAvatarURL;
-        let botembed = new Discord.RichEmbed()
-        .setDescription("Bot Information")
-        .setColor("#15f153")
-        .setThumbnail(bicon)
-        .addField("Bot Name", bot.user.username)
-        .addField("Created On", bot.user.createdAt);
-    
-        return message.channel.send(botembed);
-      }
-
-    if(cmd === `${prefix}serverinfo`){
-
-        let sicon = message.guild.iconURL;
-        let serverembed = new Discord.RichEmbed()
-        .setDescription("Server Information")
-        .setColor("#15f153")
-        .setThumbnail(sicon)
-        .addField("Server Name", message.guild.name)
-        .addField("Created On", message.guild.createdAt)
-        .addField("You Joined", message.member.joinedAt)
-        .addField("Total Members", message.guild.memberCount);
-    
-        return message.channel.send(serverembed);
-      }      
-
+    let commandfile = bot.commands.get(cmd.slice(prefix.length));
+    if(commandfile) commandfile.run(bot,message,args);  
+  
 });
 
 bot.login(botconfig.token);
